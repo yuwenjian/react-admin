94b1c2b00385d7b9311b4a74c5c5871e8d57cebb
余文建 <yuwenjian@yuwenjiandeMacBook-Pro.local>
2021-05-31 19:28:18 +0800

    first submmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {Button, message} from "antd";
+import 'antd/dist/antd.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+
+    handleClick =() =>{
+        message.success("成功！！！")
+    }
+    render() {
+        return(
+            <div>测试页面
+            <br/>
+                <Button type="'primary" onClick={this.handleClick}>按钮</Button>
+            </div>
+        )
+    }
 }
-
-export default App;
